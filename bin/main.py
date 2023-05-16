1583af09136ece7db9f512648a381e38af62e2f1
unknown <alexcombeau@outlook.fr>
2023-05-17 00:54:36 +0200

    Creating project, baseline coded

---
+++ bin/main.py
@@ -0,0 +1,30 @@
+
+from tools import *
+
+def main():
+
+    DB_entity_list, WK_entity_list = list(map(list,read_sameAs_file("dbpedia_sameAs_clean.json"))) #convert the sets back to maps
+    
+    
+    slice_size = 2000
+    sliced = property_list_cutting(DB_entity_list,slice_size) #cut the big list into slice of 100 in a 2D list
+    query_values_strings = [] #list of string properties used for VALUES in the sparql query
+    for i in sliced:
+        query_values_strings.append(query_generate_VALUES(i)) #join all prop in each bucket in a single string
+    
+    #call with sparql to get all properties used for each entities
+    for index, prop in enumerate(query_values_strings):
+        sparql_query ="""select distinct ?a ?b  where {
+        values ?a { """+prop+""" }.
+        ?a ?b ?_.
+        }  
+        """
+        result_file = "result_prop"+str(index)+".json"
+        sparql_query = re.sub(r"\n|'","",sparql_query)
+        sparql_call(sparql_query, result_file)
+        read_result_file(result_file)
+        #need wait command to process result file before calling new sparql query
+        
+    
+if __name__=="__main__":
+    main()
---
+++ bin/tools.py
@@ -0,0 +1,113 @@
+
+import sys
+import ijson
+import re
+import math
+from functools import reduce 
+import numpy as np
+import json
+import os
+
+
+def property_list_cutting(l: list ,slice_size: int ) -> list:
+    """From a list, return slice 2D with n sublist of size "slice_size"
+
+    Args:
+        l (_type_): huge 1D list
+        slice_size (_type_): 2D list composed of n "slice_size" list
+
+    Returns:
+        list : slice_size
+    """
+    index = 0
+    sliced_list = []
+    if len(l) < slice_size: return l
+    while index<len(l):
+        sliced_list.append(l[index:index+slice_size])
+        index+=slice_size
+    return sliced_list
+
+def query_generate_VALUES(slice: list) -> str:
+    """From a list return a string composed of all element of the list joined
+
+    Args:
+        slice (list): 1D list
+
+    Returns:
+        str : return a string of the type " slice[0] slice[1] slice[2] ... "
+    """
+    func = lambda acc,x : acc+" "+str(x)
+    acc = ""
+    joined_string = reduce(func, slice, acc)
+    return joined_string
+
+
+def read_sameAs_file(file: str) -> None:
+    """Generate a list of sameAs from Dbpedia and Wikidata, we keep track of how many lines we read 
+
+    Args:
+        file (str): sameAs json file name
+        nb_lines (int): number of lines we want to read
+    """
+    f_read = open(file, 'r', encoding="UTF-8")
+
+    first = "DB"
+    second = "WK"
+
+    DB_entity_list = set()
+    WK_entity_list = set()
+
+    for record in ijson.items(f_read, "results.bindings.item"):
+        DB_entity_list.add(f'<{record[first]["value"]}>')
+        WK_entity_list.add(f'<{record[second]["value"]}>')
+
+    f_read.close()
+    print(len(DB_entity_list))
+    print(len(WK_entity_list))
+    return DB_entity_list,WK_entity_list
+
+
+def read_result_file(file : str) -> None:
+    
+    f_read = open(file, 'r', encoding="UTF-8")
+
+    prop = "b"
+
+    properties_count_file = "property_support.json"
+    #if file exist open json file and load 
+    if os.path.exists(properties_count_file):
+        with open(properties_count_file) as json_file:
+            property_dictionnary = json.load(json_file)
+            
+    #else start with empty dict
+
+    else:
+        property_dictionnary = {}
+
+
+    for record in ijson.items(f_read, "results.bindings.item"):
+        item = (f'<{record[prop]["value"]}>')
+        prop_count = property_dictionnary.get(item)
+        if prop_count: property_dictionnary[item] += 1
+        else: property_dictionnary[item] = 1
+
+    f_read.close()
+    #dump new dict
+    with open(properties_count_file, "w") as fp:
+        json.dump(property_dictionnary,fp) 
+
+
+
+def sparql_call(sparql_query: str, result_file : str) -> None :
+    """Call a jar file to execute a sparql query on a database for a specified query 
+
+    Args:
+        sparql_query (str): sparql query we want to execute on a database
+    """
+    jar_path = "../soulard/QueryHDT/SparqlToJSON.jar"
+    dataset_path = "../soulard/Graphs_HDT/DBpedia/DBpedia_en.hdt"
+    result_file_name = result_file
+    result_file_path = "./RequestResults/"+result_file_name
+    hdt_command = "nohup java -Xmx120G -Xms120G -jar "+jar_path+" "+dataset_path+" \""+sparql_query+"\" > "+result_file_path+" &"
+    os.system(hdt_command)
+
